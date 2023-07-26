6139b81ae1f1e251994b8bd53059584b38209222
Crowdbotics <team@crowdbotics.com>
2023-07-26 08:19:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "real-rpg-42435",
+  url: "https://real-rpg-42435.botics.co",
+  api: "https://real-rpg-42435.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
